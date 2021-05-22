ab45dd0d23ebd8495037abf37196b4a9482a0710
yume6165 <dyruemeem1332@keio.jp>
2021-05-22 11:36:18 +0900

    unityプロジェクトの作成

---
+++ Good job manager/Assets/Scripts/ScreenManager.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScreenManager : MonoBehaviour
+{
+    public GameObject MainPrivateScreen;
+    public GameObject PTaskInputScreen;
+    public GameObject PTaskCheckScreen;
+    public GameObject PCharacterChangeScreen;
+    public GameObject PChandyScreen;
+    public GameObject PSettingScreen;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
